4c5b033db7e472929f0ad77f617f36b65d05b1df
Imdavisp <david.perezca@upb.edu.co>
2026-07-30 11:25:34 -0500

    constanteOperadoresTipos

---
+++ FundamentosProgramacionMJ10DavidP-rez/README.md
@@ -0,0 +1,2 @@
+# FundamentosProgramacionMJ10DavidP-rez
+son los ejercicios realizados en clase de fundamentos de programación
---
+++ FundamentosProgramacionMJ10DavidP-rez/VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aqui escribo el codigo fuente 
+            string nombre="David Pérez";
+            Console.WriteLine("Bienvenido"+ nombre +" al curso de fundamentos de programación");
+
+        }
+    }
+}
