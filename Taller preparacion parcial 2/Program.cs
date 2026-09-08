a79a834210ffeea3b98803aeb3b4b19f349998b0
Imdavisp <david.perezca@upb.edu.co>
2026-09-08 11:22:36 -0500

    taller preparacion paecial 2

---
+++ Taller preparacion parcial 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_preparacion_parcial_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float cal;
+
+
+            int cont = 0;
+
+
+            float acum = 0;
+
+
+            int res;
+
+
+            float prom;
+
+
+
+            do
+
+            {
+
+
+                cont++;
+
+
+                Console.WriteLine("Ingrese su nota");
+
+
+                cal = float.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Si desea agregar otra nota presione 1, de lo contrario presione 2");
+
+
+                res = int.Parse(Console.ReadLine());
+
+
+                acum += cal;
+
+
+
+            }
+
+
+            while (res == 1);
+
+
+            prom = acum / cont;
+
+
+            Console.WriteLine($"El promedio de sus notas es:{prom}");
+
+
+
+
+           
+        }
+    }
+}
