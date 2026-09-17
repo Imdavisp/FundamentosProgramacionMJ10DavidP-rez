d6f3036371dbce07d7ac50e647e45f6f9e42e7b9
Imdavisp <david.perezca@upb.edu.co>
2026-09-17 10:47:56 -0500

    Taller vectores

---
+++ taller vectores/taller vectores/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taller_vectores
+{
+    internal class Program
+
+    {
+       /* 1. Escribir un algoritmo que permita llenar un vector[15] con números enteros, y luego
+encuentre y muestre el valor máximo y mínimo de los números ingresados.
+2. Escribir un algoritmo que permita: 
+a.Crear dos vectores del mismo tamaño. 
+b.Llenarlos con números.
+c.Comparar posición por posición. 
+d.Indicar cuántos elementos son iguales.
+3. Escribir un algoritmo que permita: 
+a.Llenar un vector[20] con números enteros (positivos o negativos) ingresados por el
+usuario o generados aleatoriamente.
+b.Calcular y mostrar el promedio aritmético de todos los elementos almacenados en
+el vector.
+c.Recorrer nuevamente el vector para contar e indicar cuántos números son
+mayores que el promedio y cuántos son menores que este. 
+d.Mostrar en pantalla el vector completo junto con los resultados obtenidos.*/
+
+        static void Main(string[] args)
+        {
+
+            int[] vector = new int[15];
+
+           
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("Ingrese el número " + (i + 1) + ": ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+         
+            int maximo = vector[0];
+            int minimo = vector[0];
+
+           
+            for (int i = 1; i < 15; i++)
+            {
+                if (vector[i] > maximo)
+                {
+                    maximo = vector[i];
+                }
+
+                if (vector[i] < minimo)
+                {
+                    minimo = vector[i];
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("El número máximo es: " + maximo);
+            Console.WriteLine("El número mínimo es: " + minimo);
+
+            /*int[] vector1 = new int[10];
+            int[] vector2 = new int[10];
+
+            int iguales = 0;
+
+
+            Console.WriteLine("Ingrese los números del primer vector:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Número " + (i + 1) + ": ");
+                vector1[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("\nIngrese los números del segundo vector:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Número " + (i + 1) + ": ");
+                vector2[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vector1[i] == vector2[i])
+                {
+                    iguales++;
+                }
+            }
+
+
+            Console.WriteLine("\nCantidad de elementos iguales: " + iguales);*/
+
+        }
+
+        }
+    }
+
